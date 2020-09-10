c31f9e0bf4257fd6e6e0787a66c41fe78de7ebdd
Kyle Fredrickson <kyfredri@ucsc.edu>
2020-09-09 20:26:32 -0700

    Added evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -62,37 +62,3 @@ def evaluate_rating(inferred, truth):
 
 def evaluate_ranking(inferred, truth):
     pass
-
-def main():
-    '''
-    parser = argparse.ArgumentParser(description='Evaluate inferred.')
-
-    parser.add_argument('--inferred', metavar=['INFERRED FILE'], nargs=1, required=True, help='Path to inferred from this file.')
-    parser.add_argument('--truth', metavar=['TRUTH FILE'], nargs=1, required=True, help='Path to truth from this file.')
-
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--ratings', action='store_true', help='Evaluate inferred as ratings.')
-    group.add_argument('--rankings', action='store_true', help='Evaluate inferred as rankings.')
-
-    args = parser.parse_args()
-    '''
-
-    #inferred_path = os.path.abspath(os.path.join(THIS_DIR, args.inferred[0]))
-    inferred_path = os.path.abspath(os.path.join(THIS_DIR, '../data/predicates/model/mf_ratings_test.txt')) 
-    #truth_path = os.path.abspath(os.path.join(THIS_DIR, args.truth[0]))
-    truth_path = os.path.abspath(os.path.join(THIS_DIR, '../data/raw/test.txt'))
-
-    inferred = pd.read_csv(inferred_path, sep='\t', lineterminator='\n', names=['user_id', 'item_id', 'inferred_rating'])
-    truth = pd.read_csv(truth_path, usecols=['user_id', 'item_id', 'rating', 'user_attr', 'model_attr'], sep='\t', lineterminator='\n', converters={"user_attr": str})
-
-    #if (args.ratings):
-    if (True):
-        evaluation = evaluate_rating(inferred, truth)
-        print(', '.join([ "%s: %s" % (str(x), str(evaluation[x])) for x in evaluation.keys()]))
-    '''
-    if (args.rankings):
-        pass
-    '''
-
-if (__name__ == '__main__'):
-    main()   
