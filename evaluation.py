68cffd1c787422813b4b7cefb4f0a683e105fa27
Caleb Levy <cclevy@ucsc.edu>
2020-09-10 10:32:52 -0700

    Working accounting for null values

--- evaluation.py
+++ evaluation.py
@@ -17,35 +17,32 @@ def evaluate(predicate_dir, output_dir):
     data["rating_inferred"] = 4*data["rating_inferred"] + 1
     MSE = mean_squared_error(data["rating_truth"], data["rating_inferred"], squared=False)
     print(output_dir)
-    print(f"{MSE=}")
     MAE = mean_absolute_error(data["rating_truth"], data["rating_inferred"])
-    print(f"{MAE=}")
     F = f_stat(predicate_dir, data)
-    print(f"{F}")
+    print(f"{MSE=}, {MAE=}, {F=}")
 
 
 def f_stat(predicate_dir, data):
     item_group = pd.read_csv(predicate_dir / "eval" / "observations" / "ItemGroup.txt", sep='\t', names=["item_id", "model_attr"])
     user_group = pd.read_csv(predicate_dir / "eval" / "observations" / "UserGroup.txt", sep='\t', names=["user_id", "user_attr"])
-    data = data.merge(item_group, on="item_id").merge(user_group, on="user_id")
+    data = data.merge(item_group, on="item_id", how='left').merge(user_group, on="user_id", how='left')
     true_ratings = np.array(data["rating_truth"])
     inferred_ratings = np.array(data["rating_inferred"])
     errors = true_ratings - inferred_ratings
     data['error'] = errors
+    data.fillna('unspecified')
     data.to_csv("test.csv", index=False)
-    exit()
+    data = pd.read_csv("test.csv", converters={"model_attr": str, "user_attr": str})
     user_attrs = data["user_attr"].dropna().unique()
     model_attrs = data["model_attr"].dropna().unique()
-    print(user_attrs, model_attrs)
     M = len(user_attrs)
     N = len(model_attrs)
-    average_error = errors.mean()
+    average_error = data["error"].mean()
     V = 0
     U = 0
     for m in user_attrs:
         for n in model_attrs:
             market_segment = data[(data['user_attr'] == m) & (data['model_attr'] == n)]
-            print(m, n, market_segment.shape)
             market_segment_average_error = market_segment['error'].mean()
 
             V += market_segment.shape[0] * (market_segment_average_error - average_error) ** 2
@@ -56,7 +53,6 @@ def f_stat(predicate_dir, data):
                 u_market_segment += (error - market_segment_average_error) ** 2
 
             U += u_market_segment
-    f1, p = sm.stats.anova_lm(ols('error ~ model_attr*user_attr - model_attr - user_attr', data=data).fit()).values[0, -2:]
-    f2 = (V / (M * N - 1)) / (U / (data.shape[0] - (M * N)))
-    print(f2, f1, p)
+    f1 = (V / (M * N - 1)) / (U / (data.shape[0] - (M * N)))
+    f2, p2 = sm.stats.anova_lm(ols('error ~ model_attr*user_attr - model_attr - user_attr', data=data).fit()).values[0, -2:]
     return f1
